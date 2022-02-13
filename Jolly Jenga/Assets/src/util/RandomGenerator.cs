6801895e0d4aa6e459e826588599a9fd87eb9c52
bwallker <benbwall@gmail.com>
2022-02-13 03:12:34 +0200

    add nullable attribute to RandomGenerator

--- Jolly Jenga/Assets/src/util/RandomGenerator.cs
+++ Jolly Jenga/Assets/src/util/RandomGenerator.cs
@@ -1,8 +1,8 @@
-﻿namespace src.util
+﻿#nullable enable
+namespace src.util
 {
   internal static class RandomGenerator
   {
-    [JetBrains.Annotations.NotNullAttribute]
     private static readonly System.Random Random = new();
 
     internal static float RandomFloat(float minValue, float maxValue) =>
