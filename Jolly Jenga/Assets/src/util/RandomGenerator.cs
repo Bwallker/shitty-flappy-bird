eec3a89ff959b7774c4dc177727ee3cf3587eb85
bwallker <benbwall@gmail.com>
2022-02-16 21:28:36 +0200

    Remove textures temporarility

--- Jolly Jenga/Assets/src/util/RandomGenerator.cs
+++ Jolly Jenga/Assets/src/util/RandomGenerator.cs
@@ -7,9 +7,11 @@ namespace src.util
   {
     private static readonly Random Random = new();
 
+    /// Returns a random float between minValue (inclusive) and maxValue (exclusive)
     internal static float RandomFloat(float minValue, float maxValue) =>
             ((float) RandomGenerator.Random.NextDouble() * (maxValue - minValue)) + minValue;
-
+    /// Returns a random bool based on the probability it is true
+    /// <param></param>
     internal static bool RandomBool(double probabilityOfBeingTrue) =>
             RandomGenerator.Random.NextDouble() < probabilityOfBeingTrue;
 
