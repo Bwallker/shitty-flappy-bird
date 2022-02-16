bbef40739313369e8f1b4a0671638ffa8ba03232
bwallker <benbwall@gmail.com>
2022-02-16 20:51:31 +0200

    Add scene to repo

---
+++ Jolly Jenga/Assets/src/objects/bird/Bird.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using UnityEngine;
+
+
+namespace src.objects.bird
+{
+  public sealed class Bird : MonoBehaviour
+  {
+    public void FixedUpdate()
+    {
+      if (Input.GetKey(KeyCode.Space))
+      {
+        Debug.Log("SPACE!!!!!!!!!!!!!!!!");
+      }
+    }
+  }
+}
