6e37bdc7e1557d1b303f5838f220e58384f84eb6
bwallker <benbwall@gmail.com>
2022-02-19 05:09:58 +0200

    Add stuff

---
+++ Jolly Jenga/Assets/src/objects/obstacle/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using src.components;
+
+using System;
+
+using UnityEngine;
+
+
+namespace src
+{
+  public sealed class Obstacle : MonoBehaviour
+  {
+    private void OnTriggerEnter(Collider other)
+    {
+      var obj = other.gameObject;
+
+      if (!Tags.HasTag(obj, "Bird"))
+      {
+        return;
+      }
+
+      UI.UI.Instance!.Score();
+    }
+  }
+}
---
+++ Jolly Jenga/Assets/src/objects/obstacle/ObstaclePart.cs
@@ -0,0 +1,22 @@
+﻿using src.components;
+
+using UnityEngine;
+
+
+namespace src
+{
+  public sealed class ObstaclePart : MonoBehaviour
+  {
+    private void OnTriggerEnter(Collider other)
+    {
+      var obj = other.gameObject;
+
+      if (!Tags.HasTag(obj, "Bird"))
+      {
+        return;
+      }
+
+      UI.UI.Instance!.GameOver();
+    }
+  }
+}
